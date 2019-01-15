72cfb996548ac6d982aa1841fe7483bc72cc3328
Federico Pereiro <fpereiro@gmail.com>
2019-01-15 20:37:42 +0100

    Update provision script.

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 if [ "$2" != "confirm" ] ; then
-   echo "Must add 'confirm' to deploy to prod"
+   echo "Must add 'confirm'"
    exit 1
 fi
 if [ "$1" == "prod" ] ; then
@@ -12,8 +12,7 @@ else
 fi
 
 ssh $HOST apt-get update
-ssh $HOST DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-ssh $HOST DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
+ssh $HOST DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --with-new-pkgs
 ssh $HOST apt-get install fail2ban -y
 ssh $HOST apt-get install htop -y
 ssh $HOST "curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -"
@@ -28,5 +27,7 @@ ssh $HOST apt-get install redis-server -y
 ssh $HOST apt-get install imagemagick -y
 ssh $HOST apt-get install nginx -y
 ssh $HOST apt-get autoremove -y
+ssh $HOST apt-get clean -y
+ssh $HOST apt-get autoclean
 ssh $HOST mkdir /root/files
 ssh $HOST shutdown -r now
