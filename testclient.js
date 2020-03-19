b8d06ae2bac3aea7ed740a2cfc6291cedfe7941a
Federico Pereiro <fpereiro@gmail.com>
2020-03-19 15:46:43 +0100

    Update tests after changes.

--- testclient.js
+++ testclient.js
@@ -7,16 +7,16 @@ c.test = function (tests) {
       if (! test) return clog ('Done with tests in', teishi.time () - t, 'ms');
       var check = function () {
          var result = test [2] ();
-         if (result === false) throw new Error ('c.test', 'Test failed:', test [0]);
+         if (result === false) throw new Error ('c.test: Test failed: ' + test [0]);
          runNext (tests, k + 1);
       }
 
       clog ('c.test', 'Running test:', test [0]);
       var result = test [1] (function (wait) {
-         if (type (wait) !== 'integer') throw new Error ('c.test', 'wait parameter must be an integer but instead is', wait);
+         if (type (wait) !== 'integer') throw new Error ('c.test: wait parameter must be an integer but instead is ' + wait);
          setTimeout (check, wait);
       });
-      if (result === false) throw new Error ('c.test', 'Test failed:', test [0]);
+      if (result === false) throw new Error ('c.test: Test failed: ' + test [0]);
       if (result !== undefined) check ();
    }
    runNext (tests, 0);
@@ -37,7 +37,7 @@ c.test ([
       B.do ('logout', []);
       wait (100);
    }, function () {
-      return B.get ('State', 'view') === 'login' && window.location.hash === '#/login';
+      return B.get ('State', 'page') === 'login' && window.location.hash === '#/login';
    }],
    ['Invalid login', function (wait) {
       c.set ('#auth-username', {value: 'fpereiro'});
@@ -45,7 +45,7 @@ c.test ([
       B.do ('login', []);
       wait (300);
    }, function () {
-      return B.get ('State', 'notify') !== undefined && B.get ('State', 'view') === 'login' && window.location.hash === '#/login';
+      return B.get ('State', 'snackbar') !== undefined && B.get ('State', 'page') === 'login' && window.location.hash === '#/login';
    }],
    ['Login', function (wait) {
       c.set ('#auth-username', {value: 'fpereiro'});
@@ -53,27 +53,27 @@ c.test ([
       B.do ('login', []);
       wait (300);
    }, function () {
-      return B.get ('State', 'view') === 'pictures' && window.location.hash === '#/pictures';
+      return B.get ('State', 'page') === 'pics' && window.location.hash === '#/pics';
    }],
    ['Try navigating to a non-existing view #1', function (wait) {
       window.location.hash = '#/foo'
       // We wait until the hashchange event is processed.
       wait (0);
    }, function () {
-      return B.get ('State', 'view') === 'pictures' && window.location.hash === '#/pictures';
+      return B.get ('State', 'page') === 'pics' && window.location.hash === '#/pics';
    }],
    ['Try navigating to a non-existing view #2', function (wait) {
-      B.do ('set', ['State', 'view'], 'foo');
+      B.do ('set', ['State', 'page'], 'foo');
       return true;
    }, function () {
-      return B.get ('State', 'view') === 'pictures' && window.location.hash === '#/pictures';
+      return B.get ('State', 'page') === 'pics' && window.location.hash === '#/pics';
    }],
    ['Try navigating to an existing view', function (wait) {
       window.location.hash = '#/upload'
       // We wait until the hashchange event is processed.
       wait (0);
    }, function () {
-      return B.get ('State', 'view') === 'upload' && window.location.hash === '#/upload';
+      return B.get ('State', 'page') === 'upload' && window.location.hash === '#/upload';
    }],
    ['Logout by clicking the logout button', function (wait) {
       var dropdownItems = c ('li.account-sub-menu__item a');
@@ -81,7 +81,7 @@ c.test ([
       c.fire (dropdownItems [1], 'click');
       wait (100);
    }, function () {
-      return B.get ('State', 'view') === 'login' && B.get ('Data', 'csrf') === false;
+      return B.get ('State', 'page') === 'login' && B.get ('Data', 'csrf') === false;
    }],
    ['Set invalid redirect', function (wait) {
       window.location.hash = '#/foo'
@@ -95,13 +95,13 @@ c.test ([
       B.do ('login', []);
       wait (300);
    }, function () {
-      return B.get ('State', 'view') === 'pictures' && window.location.hash === '#/pictures';
+      return B.get ('State', 'page') === 'pics' && window.location.hash === '#/pics';
    }],
    ['Logout after invalid redirect', function (wait) {
       B.do ('logout', []);
       wait (100);
    }, function () {
-      return B.get ('State', 'view') === 'login' && window.location.hash === '#/login';
+      return B.get ('State', 'page') === 'login' && window.location.hash === '#/login';
    }],
    ['Set valid redirect', function (wait) {
       window.location.hash = '#/upload'
@@ -115,23 +115,32 @@ c.test ([
       B.do ('login', []);
       wait (300);
    }, function () {
-      return B.get ('State', 'view') === 'upload' && window.location.hash === '#/upload';
+      return B.get ('State', 'page') === 'upload' && window.location.hash === '#/upload';
    }],
-   ['Check that store is cleared and that notify is fired on 403 request', function (wait) {
+   ['Check that store is cleared and that snackbar is fired on 403 request', function (wait) {
       B.do ({}, 'post', 'auth/logout', {}, {}, function () {
          B.do ({}, 'get', 'foo', {}, '');
       });
       wait (150);
    }, function () {
-      if (! B.get ('State', 'notify')) return clog ('No error message shown.');
-      if (B.get ('State', 'notify', 'message') !== 'Your session has expired. Please login again.') return clog ('Wrong error message shown.');
-      return B.get ('State', 'view') === 'login' && window.location.hash === '#/login';
+      if (! B.get ('State', 'snackbar')) return clog ('No error message shown.');
+      if (B.get ('State', 'snackbar', 'message') !== 'Your session has expired. Please login again.') return clog ('Wrong error message shown.');
+      return B.get ('State', 'page') === 'login' && window.location.hash === '#/login';
    }],
-   // *** NOTIFY ***
-   ['Close notify message', function (wait) {
+   // *** snackbar ***
+   ['Close snackbar message', function (wait) {
       c.fire (c ('.snackbar__close') [0], 'click');
       wait (0);
    }, function () {
-      return B.get ('State', 'notify') === undefined;
+      return B.get ('State', 'snackbar') === undefined;
+   }],
+   // *** pictures ***
+   ['Login', function (wait) {
+      c.set ('#auth-username', {value: 'fpereiro'});
+      c.set ('#auth-password', {value: password});
+      B.do ('login', []);
+      wait (300);
+   }, function () {
+      return B.get ('State', 'page') === 'pics' && window.location.hash === '#/pics';
    }],
 ]);
