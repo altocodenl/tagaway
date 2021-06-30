a2f4e04364657596bdd08e2e7774819583eef8c1
Federico Pereiro <fpereiro@gmail.com>
2021-06-30 20:28:43 +0200

    Debugging tools in client.

--- client.js
+++ client.js
@@ -3,6 +3,15 @@
 var dale = window.dale, teishi = window.teishi, lith = window.lith, c = window.c, B = window.B;
 var type = teishi.type, clog = teishi.clog, media = lith.css.media, style = lith.css.style;
 
+window.addEventListener ('keydown', function (ev) {
+   var code = (ev || document.event).keyCode;
+   if (code !== 75) return;
+   ev.preventDefault ();
+   var query = prompt ('Search the eventlog');
+   if (query === null && c ('#eventlog')) return c ('#eventlog').parentNode.removeChild (c ('#eventlog'));
+   B.eventlog (query);
+});
+
 // *** CSS ***
 
 var CSS = {
@@ -2464,13 +2473,13 @@ B.mrespond ([
       B.call (x, 'set', ['State', 'snackbar'], {color: colors [x.path [0]], message: snackbar, timeout: timeout});
    }],
    [/^get|post$/, [], {match: H.matchVerb}, function (x, headers, body, cb) {
-      var path = x.path [0], noCSRF = path === 'requestInvite' || (path.match (/^auth/) && ['auth/logout', 'auth/delete', 'auth/changePassword'].indexOf (path) === -1);
-      // CSRF protection
+      var t = Date.now (), path = x.path [0], noCSRF = path === 'requestInvite' || (path.match (/^auth/) && ['auth/logout', 'auth/delete', 'auth/changePassword'].indexOf (path) === -1);
       if (x.verb === 'post' && ! noCSRF) {
          if (type (body, true) === 'formdata') body.append ('csrf', B.get ('Data', 'csrf'));
          else                                  body.csrf = B.get ('Data', 'csrf');
       }
       c.ajax (x.verb, x.path [0], headers, body, function (error, rs) {
+         B.call (x, 'ajax', x.verb, x.path, Date.now () - t);
          if (path !== 'csrf' && ! path.match (/^auth/) && error && error.status === 403) {
             B.call (x, 'reset', 'store', true);
             return B.call (x, 'snackbar', 'red', 'Your session has expired. Please login again.');
