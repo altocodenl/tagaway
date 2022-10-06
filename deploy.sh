b4846308386c6ea2e0cf3acfe3eee6d1832b6948
Federico Pereiro <fpereiro@gmail.com>
2022-10-06 21:37:13 +0200

    Accelerate deploy using rsync.

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ else
 fi
 
 FOLDER="acpic"
-TAR="acpic.tar.gz"
 
 if [ "$2" == "client" ] ; then
    scp client.js $HOST:$FOLDER
@@ -52,16 +51,7 @@ if [ "$2" == "checkConsistency" ] ; then
    exit 0
 fi
 
-if [ "$2" == "fast" ] ; then
-   cd .. && tar --exclude="$FOLDER/*.swp" --exclude="$FOLDER/node_modules" --exclude="$FOLDER/.git" --exclude="$FOLDER/test" -czvf $TAR $FOLDER
-else
-   cd .. && tar --exclude="$FOLDER/*.swp" --exclude="$FOLDER/node_modules" -czvf $TAR $FOLDER
-fi
-
-scp $TAR $HOST:
-ssh $HOST tar xzvf $TAR
+rsync -av . $HOST:$FOLDER
 ssh $HOST chown -R root /root/$FOLDER
 echo "main = node server $1" | ssh $HOST "cat >> $FOLDER/mongroup.conf"
 ssh $HOST "cd $FOLDER && npm i --no-save --production && mg restart"
-ssh $HOST rm $TAR
-rm $TAR
