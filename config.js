394d8603647c63b7dedcc368467da7169a857525
Federico Pereiro <fpereiro@gmail.com>
2023-11-24 21:23:58 +0100

    Update limit of users

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ module.exports = {
    domain: DOMAIN,
    cookieName: 'acpic' + (ENV ? '-' + ENV : ''),
    allowedFormats: ['image/jpeg', 'image/png', 'image/bmp', 'image/heic', 'image/heif', 'image/gif', 'image/tiff', 'image/webp', 'video/mp4', 'video/quicktime', 'video/3gpp', 'video/avi', 'video/x-msvideo', 'video/webm', 'video/x-ms-wmv', 'video/x-m4v', 'video/mpeg', 'video/x-flv'],
-   maxUsers: 50,
+   maxUsers: 100,
    thumbSizes: {S: 300, M: 900},
    port: ENV ? 1427 : 8000,
    basepath: ENV ? '/root/files/acpic' : '/tmp',
